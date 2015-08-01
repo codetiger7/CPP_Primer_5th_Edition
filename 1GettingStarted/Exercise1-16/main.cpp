18ec72125012583ba115f048b08f28c28698dc91
codetiger7 <brianopedal@MacBook-Pro.local>
2015-08-01 15:29:55 +0200

    commit

---
+++ 1GettingStarted/Exercise1-16/main.cpp
@@ -0,0 +1,14 @@
+// Exercise 1.16
+// Write your own version of a program that prints the sum of
+// a set of integers read from cin
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
+
