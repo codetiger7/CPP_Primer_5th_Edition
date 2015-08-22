1c50f8c7f9b3d9f377e65d70b4ed5d0ab3cc3422
codetiger7 <brianopedal@MacBook-Pro.local>
2015-08-22 16:07:38 +0200

    f

---
+++ 4.Expressions/Exercise4-14/main.cpp
@@ -0,0 +1,20 @@
+// Exercise 4.14
+// explain what happens in the if tests
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int i = 4;
+   // tries to assign to rv error
+//   if ( 42 = i)
+//      ;
+
+
+   // assigns 42 to i, checks if i is a value other than 0 before execution if loop
+   if ( i = -23)
+      cout << "perfect!" << endl;
+   return 0;
+}
+
