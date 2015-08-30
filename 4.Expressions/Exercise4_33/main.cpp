0e60a562d62708de109c4efae55ea96dbd273d7a
codetiger7 <brianopedal@MacBook-Pro.local>
2015-08-30 18:36:09 +0200

    sd

--- 4.Expressions/Exercise4_33/main.cpp
+++ 4.Expressions/Exercise4_33/main.cpp
@@ -3,13 +3,52 @@
 
 
 
-someValue ? ++x, ++y : --x, --y;
+// someValue ? ++x, ++y : --x, --y;
 
 // someValue is evaluated as a bool expression, if it is zero (i.e. false)
 
-// then x and y is decremented by one unit each
+// then x is decremented by one unit
 
-// if someValue is any value other than zero (i.e. true), then x and y is incremented by one
+// if someValue is any value other than zero (i.e. true), then x and y is incremented
+
+// No matter what someValue is, y is always decremented by one this because
+// the comma operator left to right is weaker than the conditional operator Right to left
+// so expression is evaluated as: (somevalue ? ++x, ++y : --x), --y;
+// see: https://github.com/Mooophy/Cpp-Primer/tree/master/ch04
+// this means that when someValue is true first increases by one, then decreases by one
+// effectively standing still
 
 
 
+// demo
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+   int someValue = 0;
+   int x = 0;
+   int y = 0;
+
+   someValue ? ++x, ++y : --x, --y;
+
+   cout << "someValue: " << someValue << endl;
+   cout << "x: " << x << endl;
+   cout << "y: " << y << endl;
+
+
+   someValue = 1;
+
+   someValue ? ++x, ++y : --x, --y;
+
+
+   cout << "someValue: " << someValue << endl;
+   cout << "x: " << x << endl;
+   cout << "y: " << y << endl;
+
+
+
+
+   return 0;
+}
