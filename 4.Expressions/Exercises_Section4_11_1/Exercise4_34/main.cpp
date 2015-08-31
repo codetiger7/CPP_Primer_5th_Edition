54ebae91a4f8b235c870bdc76f3c55402ac5c6c3
codetiger7 <brianopedal@MacBook-Pro.local>
2015-08-31 18:18:53 +0200

    more exercises

---
+++ 4.Expressions/Exercises_Section4_11_1/Exercise4_34/main.cpp
@@ -0,0 +1,13 @@
+// Exercise 4.34
+// Given the variable definitions in this section
+// explain what conversion take place in the following expressions
+
+ a) if(val)
+ 0 is evaluated as "true" , any other value as "false"
+
+ b) dval = fval + ival;
+ int is converted to double, fval to double
+
+ c) dval + ival * cval
+ char is converted to int two ints multiplied and result converted to double
+ then added to double.
